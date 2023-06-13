abf23e807f312412d25dc2ae8d60f5daf0048aad
jdenholm <j.denholm.2017@gmail.com>
2023-06-13 22:16:24 +0100

    :recyle: :construction: Working on penguin dataset

--- src/ml_workshop/_penguins.py
+++ src/ml_workshop/_penguins.py
@@ -1,7 +1,11 @@
 """Penguins dataset."""
+from typing import Optional, List, Dict, Tuple, Any
+
 from torch.utils.data import Dataset
+from torchvision.transforms import Compose
 
 from pandas import DataFrame
+
 
 from palmerpenguins import load_penguins
 
@@ -18,14 +22,62 @@ class PenguinDataset(Dataset):
 
     """
 
-    def __init__(self):
+    def __init__(
+        self,
+        input_keys: List[str],
+        target_keys: str,
+        train: bool,
+        x_tfms: Optional[Compose] = None,
+        y_tfms: Optional[Compose] = None,
+    ):
         """Build ``PenguinDataset``."""
-        self._data = _load_penguin_data()
-        self._valid = self._data.groupby(by=["species", "sex"]).sample(
-            n=10,
-            random_state=123,
-        )
-        self._train = self._data.loc[~self._data.index.isin(self._valid.index)]
+        self.input_keys = input_keys
+        self.target_keys = target_keys
+
+        self.full_df = _load_penguin_data()
+        self.split = _split_data(self.full_df)["train" if train is True else "valid"]
+
+        self.x_tfms = x_tfms
+        self.y_tfms = y_tfms
+
+    def __len__(self) -> int:
+        """Return the length of requested split.
+
+        Returns
+        -------
+        int
+            The number of items in the dataset.
+
+        """
+        return len(self.split)
+
+    def __getitem__(self, idx: int) -> Tuple[Any, Any]:
+        """Return an input-target pair.
+
+        Parameters
+        ----------
+        idx : int
+            Index of the input-target pair to return.
+
+        Returns
+        -------
+        in_feats : Any
+            Inputs.
+        target : Any
+            Targets.
+
+        """
+        feats = tuple(self.split.iloc[idx][self.input_keys])
+
+        tgts = tuple(self.split.iloc[idx][self.target_keys])
+
+        if self.x_tfms is not None:
+            feats = self.x_tfms(feats)
+
+        if self.y_tfms is not None:
+            tgts = self.y_tfms(tgts)
+
+        return feats, tgts
 
 
 def _load_penguin_data() -> DataFrame:
@@ -45,3 +97,30 @@ def _load_penguin_data() -> DataFrame:
     )
     data.sex = (data.sex == "male").astype(float)
     return data
+
+
+def _split_data(penguin_df: DataFrame) -> Dict[str, DataFrame]:
+    """Split the ``penguin_df`` into a training and validation set.
+
+    Parameters
+    ----------
+    penguin_df : DataFrame
+        The full penguin data set.
+
+    Returns
+    -------
+    Dict[str, DataFrame]
+        Dictionary holding the ``"train"`` and ``"valid"`` splits. The valid
+        split has 10 females and 10 males of each species, and the training
+        split contains the rest of the dataset.
+
+    """
+    valid_df = penguin_df.groupby(by=["species", "sex"]).sample(
+        n=10,
+        random_state=123,
+    )
+
+    # The training items are simply the items *not* in the valid split
+    train_df = penguin_df.loc[~penguin_df.index.isin(valid_df.index)]
+
+    return {"train": train_df, "valid": valid_df}
