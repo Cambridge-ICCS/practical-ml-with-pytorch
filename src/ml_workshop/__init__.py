e9faee9852186034e3372cd5164b7528efb9c219
K. Harrison <kh296@users.noreply.github.com>
2025-08-29 21:12:44 +0100

    Made exercises and solutions more robust for torch with Intel extensions

--- src/ml_workshop/__init__.py
+++ src/ml_workshop/__init__.py
@@ -1,4 +1,5 @@
 """Init for ``ml_workshop`` package."""
 
+from ml_workshop._xpu import xpu_is_available
 from ml_workshop._penguins import PenguinDataset
 from ml_workshop._ellipse import EllipseDataset
---
+++ src/ml_workshop/_xpu.py
@@ -0,0 +1,14 @@
+"""
+PyTorch import for XPUs (Intel GPUs).
+
+Performing the import before any other imports can avoid conflicts
+among shared-object libraries.
+"""
+import torch
+try:
+    import intel_extension_for_pytorch as ipex
+except ImportError:
+    pass
+
+xpu_is_available = getattr(
+        getattr(torch, "xpu", None), "is_available", lambda: False)()
